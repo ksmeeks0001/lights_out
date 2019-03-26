75796487f290a35fbedd92b83433aa42d7cbc0fd
Kevin Smeeks <32853934+ksmeeks0001@users.noreply.github.com>
2019-03-26 14:55:21 -0400

    Animations added

--- lights_out.js
+++ lights_out.js
@@ -1,13 +1,18 @@
-//5X5 matrix for the board.
+//3X3 matrix for the board.
 // 0="off"     1="on"
 let board = [[0,0,0,0,0],
              [0,0,0,0,0],
              [0,0,0,0,0],
              [0,0,0,0,0],
              [0,0,0,0,0]];
-
 let level_count = document.getElementById("level_count");
+
+
 let blocks = document.getElementsByClassName("block");
+
+
+
+
 
 for (let i=0; i<25; i++) {
     blocks[i].addEventListener("click", function () {
@@ -32,7 +37,7 @@ for (let i=0; i<25; i++) {
     })
 }
 
-levelUp();
+animate();
 
 function toggle(block)
 {
@@ -50,6 +55,7 @@ function toggle(block)
         board[i][j] = 0;
     }
 }
+//check for all blocks toggled 0
 function checkForWin()
 {
     for (let i=0; i<5; i++)
@@ -60,15 +66,48 @@ function checkForWin()
     }
     alert("Level Passed!");
     level_count.innerText = parseInt(level_count.innerText) +1;
-    levelUp();
+    animate();
 }
+//get current level and set up game
 function levelUp() {
     let level = parseInt(level_count.innerText);
     for (let i=0; i<level; i++)
     {
-        let random = Math.floor(Math.random() * 25)
+        let random = Math.floor(Math.random() * 25);
         blocks[random].click();
     }
 
 }
+
+
+function animate(iter = 24) {
+    flash(blocks[iter], blocks[iter].style.backgroundColor);
+    if (iter+1 === parseInt(level_count.innerText)) {
+        setTimeout(function () {
+            flash(blocks[iter],blocks[iter].style.backgroundColor);
+        }, 115);
+        setTimeout(function () {
+            flash(blocks[iter],blocks[iter].style.backgroundColor);
+        }, 230);
+        setTimeout(function(){levelUp();},500);
+        return 0;
+    }
+
+    else if  (iter > 0) {
+        setTimeout(function() {animate(iter -1)}, 115);
+    } else {
+        setTimeout(function(){levelUp();},500);
+
+
+    }
+
+}
+
+
+function flash(block, color){
+        toggle(block);
+        setTimeout(toggle, 150, block);
+
+}
+
 
